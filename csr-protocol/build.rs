d471cac3afe77410d58476b6a388db848eb43fa3
Deon Poncini <deonponcini@gmail.com>
2024-09-24 19:16:12 -0700

    Move protos under protocol folder

--- csr-protocol/build.rs
+++ csr-protocol/build.rs
@@ -5,9 +5,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     // setup the protobuf compiler from source
     env::set_var("PROTOC", protobuf_src::protoc());
 
-    println!("cargo:rerun-if-changed=../protos/csr.proto");
+    println!("cargo:rerun-if-changed=protos/csr.proto");
 
     // build our grpc service
-    tonic_build::compile_protos("../protos/csr.proto")?;
+    tonic_build::compile_protos("protos/csr.proto")?;
     Ok(())
 }
